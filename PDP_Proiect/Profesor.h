fa93c39208c69d38164ab3e29ee13afc43aadb0e
lucaciobanu <61873409+lucaciobanu@users.noreply.github.com>
2021-01-12 17:47:17 +0200

    Create Profesor.h

---
+++ PDP_Proiect/Profesor.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Disponibilitate.h"
+#include <iostream>
+class Profesor {
+    Disponibilitate d;
+    string nume;
+
+public:
+    Profesor(string nume) {
+        this->nume = nume;
+    }
+    ~Profesor() {
+        cout << __FUNCTION__ << this << " has been destroyed " << endl;
+    }
+    bool reservaProfesor(const string& ziua, int ora) {
+        return d.rezerva(ziua, ora);
+    }
+    bool isAvailable(const string& ziua, int ora) {
+        return d.isAvailable(ziua, ora);
+    }
+    string getName() {
+        return this->nume;
+    }
+};
